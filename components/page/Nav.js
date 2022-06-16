c33c88e9256d87e24da78529392c41620f2519b0
mhmadwrekat <mhmmadwreekkat@gmail.com>
2022-06-16 15:45:24 +0300

    update

--- components/page/Nav.js
+++ components/page/Nav.js
@@ -103,6 +103,27 @@ const Nav = ({ showCategory, all_news }) => {
       id: 3,
     },
   ]
+  // useEffect(() => {
+  //   all_news?.map((item) => {
+  //     // item?.is_subscribed !== null &&
+  //     if (item?.is_subscribed === true) {
+  //       subscribe_item.push({
+  //         name: item?.section_name,
+  //         link: `#${item?.section_name}`,
+  //         id: count++,
+  //       })
+  //       // console.log('yes -> ', item?.section_name)
+  //     }
+  //     if (item?.is_subscribed === false) {
+  //       unsubscribe_item.push({
+  //         name: item?.section_name,
+  //         link: `#${item?.section_name}`,
+  //         id: count++,
+  //       })
+  //       // console.log('No -> ', item?.section_name)
+  //     }
+  //   })
+  // }, [all_news])
   all_news?.map((item) => {
     // item?.is_subscribed !== null &&
     if (item?.is_subscribed === true) {
@@ -250,7 +271,10 @@ const Nav = ({ showCategory, all_news }) => {
                         key={item.id}
                         className="flex items-center justify-center gap-3"
                       >
-                        <a className={`w-28 lg:w-32`} href={item.link}>
+                        <a
+                          className={`w-28 rounded-full lg:w-32`}
+                          href={item.link}
+                        >
                           {item.name}
                         </a>
                       </div>
@@ -258,7 +282,7 @@ const Nav = ({ showCategory, all_news }) => {
                   })}
                 </div>
               </div>
-              {subscribe_item.length > 0 && (
+              {subscribe_item?.length > 0 && (
                 <div className="mx-2 flex justify-start rounded-full border-3 border-Purp200 pl-3">
                   {/* <img
               src="./assest/images/additional.jpg"
@@ -277,14 +301,17 @@ const Nav = ({ showCategory, all_news }) => {
                       clipRule="evenodd"
                     />
                   </svg>
-                  <div className="flex bg-Purp100 py-0 px-0 first:mr-0 first:pr-0 lg:first:mr-0 lg:first:pr-0">
-                    {subscribe_item.map((item) => {
+                  <div className="flex py-0 px-0 first:mr-0 first:pr-0 lg:first:mr-0 lg:first:pr-0">
+                    {subscribe_item?.map((item) => {
                       return (
                         <div
                           key={item.id}
-                          className="flex items-center justify-center gap-3"
+                          className="mr-5 flex w-fit items-center justify-center gap-3"
                         >
-                          <a className={`w-28 lg:w-32`} href={item.link}>
+                          <a
+                            className={`w-28 rounded-full lg:w-32`}
+                            href={item.link}
+                          >
                             {item.name}
                           </a>
                         </div>
@@ -293,16 +320,19 @@ const Nav = ({ showCategory, all_news }) => {
                   </div>
                 </div>
               )}
-              {unsubscribe_item.length > 0 && (
-                <div className="mx-2 flex h-12 justify-start rounded-full border-3 border-Purp200 px-3">
-                  <div className="flex bg-Purp100 py-0 px-0 first:mr-0 first:pr-0 lg:first:mr-0 lg:first:pr-0">
-                    {unsubscribe_item.map((item) => {
+              {unsubscribe_item?.length > 0 && (
+                <div className="mx-2 flex justify-start rounded-full border-3 border-Purp200 px-3">
+                  <div className="flex py-0 px-0 first:mr-0 first:pr-0 lg:first:mr-0 lg:first:pr-0">
+                    {unsubscribe_item?.map((item) => {
                       return (
                         <div
                           key={item.id}
-                          className="flex items-center justify-center gap-0"
+                          className="mr-5 flex w-fit items-center justify-center gap-3"
                         >
-                          <a className={`w-28 py-1 lg:w-32`} href={item.link}>
+                          <a
+                            className={`w-36 rounded-full py-1 lg:w-32 lg:py-1.5`}
+                            href={item.link}
+                          >
                             {item.name}
                           </a>
                         </div>
@@ -313,6 +343,10 @@ const Nav = ({ showCategory, all_news }) => {
               )}
             </div>
           </section>
+          {subscribe_item.map((item) => {
+            console.log(subscribe_item)
+          })}
+          {subscribe_item.length > 0 && console.log(subscribe_item.length)}
         </section>
       ) : (
         <div className="mx-auto w-11/12 border-t-2 border-Purp100"></div>
@@ -322,6 +356,100 @@ const Nav = ({ showCategory, all_news }) => {
 }
 
 export default Nav
+/*
+  {showCategory ? (
+        <section className="sticky top-0 left-0 z-50 w-screen">
+          <section className="flex w-screen justify-start bg-Purp100 py-0 text-center font-TSbold text-base text-white lg:text-lg">
+            <div className="mx-auto flex w-full items-center justify-start overflow-x-auto pb-1 pt-1.5 lg:pb-0 lg:pt-0">
+              <div className="mx-2 my-2.5 ml-3 flex justify-start rounded-full border-3 border-Purp200">
+      
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-8 w-8 rounded-full bg-Purp300 lg:h-10 lg:w-10"
+                  viewBox="0 0 20 20"
+                  fill="#695CAD"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-2 0c0 .993-.241 1.929-.668 2.754l-1.524-1.525a3.997 3.997 0 00.078-2.183l1.562-1.562C15.802 8.249 16 9.1 16 10zm-5.165 3.913l1.58 1.58A5.98 5.98 0 0110 16a5.976 5.976 0 01-2.516-.552l1.562-1.562a4.006 4.006 0 001.789.027zm-4.677-2.796a4.002 4.002 0 01-.041-2.08l-.08.08-1.53-1.533A5.98 5.98 0 004 10c0 .954.223 1.856.619 2.657l1.54-1.54zm1.088-6.45A5.974 5.974 0 0110 4c.954 0 1.856.223 2.657.619l-1.54 1.54a4.002 4.002 0 00-2.346.033L7.246 4.668zM12 10a2 2 0 11-4 0 2 2 0 014 0z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <div className="flex py-0 px-0 first:mr-0 first:pr-0 lg:first:mr-0 lg:first:pr-0">
+                  {media_item.map((item) => {
+                    return (
+                      <div
+                        key={item.id}
+                        className="flex w-fit items-center justify-center gap-0"
+                      >
+                        <a className={`mr-8 py-1 lg:w-full`} href={item.link}>
+                          {item.name}
+                        </a>
+                      </div>
+                    )
+                  })}
+                </div>
+              </div>
+              {subscribe_item?.length > 0 && (
+                <div className="mx-2 flex justify-start rounded-full border-3 border-Purp200 pl-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-8 w-8 rounded-full bg-Purp300 lg:h-10 lg:w-10"
+                    viewBox="0 0 20 20"
+                    fill="#695CAD"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      className="rounded-full"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <div className="flex py-0 px-0 first:mr-0 first:pr-0 lg:first:mr-0 lg:first:pr-0">
+                    {subscribe_item?.map((item) => {
+                      return (
+                        <div
+                          key={item.id}
+                          className="flex w-fit items-center justify-center gap-3"
+                        >
+                          <a className={`mr-8 py-1 lg:w-full`} href={item.link}>
+                            {item.name}
+                          </a>
+                        </div>
+                      )
+                    })}
+                  </div>
+                </div>
+              )}
+              {unsubscribe_item?.length > 0 && (
+                <div className="mx-2 flex justify-start rounded-full border-3 border-Purp200 px-3">
+                  <div className="flex py-0 px-0 first:mr-0 first:pr-0 lg:first:mr-0 lg:first:pr-0">
+                    {unsubscribe_item?.map((item) => {
+                      return (
+                        <div
+                          key={item.id}
+                          className="flex w-fit items-center justify-center gap-0"
+                        >
+                          <a className={`mr-8 py-1 lg:w-full`} href={item.link}>
+                            {item.name}
+                          </a>
+                        </div>
+                      )
+                    })}
+                  </div>
+                </div>
+              )}
+            </div>
+          </section>
+          {subscribe_item.map((item) => {
+            console.log(subscribe_item)
+          })}
+          {subscribe_item.length > 0 && console.log(subscribe_item.length)}
+        </section>
+      ) : (
+        <div className="mx-auto w-11/12 border-t-2 border-Purp100"></div>
+      )}
+*/
 /*
             {/* {active ? (
                   <button
