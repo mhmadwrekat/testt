4d32195fcd5da8784a1ab909b57d458d22183a9d
mhmadwrekat <mhmmadwreekkat@gmail.com>
2022-06-16 13:33:40 +0300

    update

--- components/appleTemplate/childComponent/ReadImpression.js
+++ components/appleTemplate/childComponent/ReadImpression.js
@@ -9,22 +9,22 @@ const ReadImpression = ({ user_id, stories }) => {
     unobserveOnEnter: true,
     // For performance perspective, use the largest tolerable value as much as possible // (ms)
     delay: 15000,
-    onEnter: () => {
-      let url = `https://734slo1j48.execute-api.us-east-2.amazonaws.com/prod`
-      let data = {
-        body: {
-          user: user_id,
-          stories: stories,
-        },
-      }
-      axios.post(url, data).then((res) => {
-        // console.log(data)
-        // console.log(res)
-        // console.log(data)
-        // console.log('Read')
-      })
-      // console.log(Data)
-    },
+    // onEnter: () => {
+    //   let url = `https://734slo1j48.execute-api.us-east-2.amazonaws.com/prod`
+    //   let data = {
+    //     body: {
+    //       user: user_id,
+    //       stories: stories,
+    //     },
+    //   }
+    //   axios.post(url, data).then((res) => {
+    //     // console.log(data)
+    //     // console.log(res)
+    //     // console.log(data)
+    //     // console.log('Read')
+    //   })
+    //   // console.log(Data)
+    // },
     // onLeave: () => {console.log('None')},
   })
   return (
