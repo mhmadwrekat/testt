ab4db659117cf544ef1eb0f53df927d8ebc307f5
mhmadwrekat <mhmmadwreekkat@gmail.com>
2022-06-28 10:56:24 +0300

    update

--- pages/_app.js
+++ pages/_app.js
@@ -114,18 +114,6 @@ function MyApp({ Component, pageProps }) {
           type: 'website',
         }}
       />
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${ANALYTICS}`}
-      />
-      <Script strategy="lazyOnload">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${ANALYTICS}');
-        `}
-      </Script>
       <Component {...pageProps} country_code={country_code} user_id={user_id} />
     </React.Fragment>
   )
